938268a03b86873635ceb0ebffb09e58edccf4b1
flies <a.rzadkowolski@gmail.com>
2018-12-04 01:00:07 +0100

    nginx backup

--- roles/backup/templates/backup-nginx-rdiff.sh
+++ roles/backup/templates/backup-nginx-rdiff.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-nice /usr/bin/rdiff-backup --remote-schema "ssh -i ~/.ssh/backup.priv -C -p22222 %s rdiff-backup --server" -v3 \
-    --include /home/flies \
-    --exclude "**" backup@78.11.99.66::/ /var/backups/BigMike/rdiff
+
+nice /usr/bin/rdiff-backup --no-acls --preserve-numerical-ids --remote-schema "ssh -i ~/.ssh/backup.priv -C -p22222 %s sudo rdiff-backup --server" -v3 \
+   --include /etc \
+   --include /opt --exclude /opt/libreoffice --exclude /opt/phpfarm/src --exclude /opt/phpfarm/inst/sessions \
+   --exclude "**" \
+    / \
+   backup@78.11.99.66::/var/backups/BigMike/fs
+
+
 
 
 
